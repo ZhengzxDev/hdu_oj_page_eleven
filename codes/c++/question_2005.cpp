cab1aa3aa9e255f2da85bfd3489b3964893e61ae
ZhengzxDev <56790096+ZhengzxDev@users.noreply.github.com>
2022-02-23 18:19:56 +0800

    Update question_2005.cpp

--- codes/c++/question_2005.cpp
+++ codes/c++/question_2005.cpp
@@ -8,7 +8,15 @@ int days[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
 int main(){
     int year,month,day;
     while(scanf("%d/%d/%d",&year,&month,&day)!=EOF){
-        if(year%4==0 || year%400==0){
+        if(year%100==0){
+            if(year%400==0){
+                days[1]=29;
+            }
+            else{
+                days[1]=28;
+            }
+        }
+        else if(year%4==0){
             days[1]=29;
         }
         else{
